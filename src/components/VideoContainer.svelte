6a0bee054553cd766eb14ee8c9b0004233e7d14d
Sergio Forés <sergiofores@gmail.com>
2020-12-17 10:50:25 +0100

    cambios dom 17 dic

--- src/components/VideoContainer.svelte
+++ src/components/VideoContainer.svelte
@@ -20,7 +20,7 @@
         }
 
         video {
-            min-width: 100%;
+            width: 100%;
             /* height: 100%; */
             object-fit: cover;
             mix-blend-mode: luminosity;
